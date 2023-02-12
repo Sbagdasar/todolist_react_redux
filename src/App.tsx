07e89f7d55c19cb76dd066d4a1530dd7848fe5cf
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-02-12 23:51:09 +0300

    feat: remove task feature added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./components/Todolist";
 
@@ -8,15 +8,19 @@ export type TaskType = {
     isDone: boolean
 }
 export function App() {
-    const tasks:TaskType[] = [
+    const [tasks, setTasks] = useState<TaskType[]>([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false}
-    ]
+    ])
+
+    const removeTask = (taskId: number)=>{
+        setTasks(tasks.filter(t=> t.id !== taskId))
+    }
 
     return (
         <div className="App">
-            <Todolist title={'What to buy'} tasks={tasks}/>
+            <Todolist title={'What to buy'} tasks={tasks} removeTask={removeTask}/>
         </div>
     );
 }
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {TaskType} from "../App";
 
 export type TodolistPropsType = {
-    title:string
-    tasks:TaskType[]
+    title: string
+    tasks: TaskType[]
+    removeTask: (taskId:number)=>void
 }
-export const Todolist = (props:TodolistPropsType) => {
+export const Todolist = (props: TodolistPropsType) => {
     return (
         <div>
             <h3>{props.title}</h3>
@@ -16,7 +17,11 @@ export const Todolist = (props:TodolistPropsType) => {
             <ul>
                 {
                     props.tasks.map(task => {
-                        return  <li key={task.id}><input type="checkbox" checked={task.isDone}/> <span>{task.title}</span></li>
+                        return <li key={task.id}>
+                            <input type="checkbox" checked={task.isDone}/>
+                            <span>{task.title}</span>
+                            <button onClick={()=>props.removeTask(task.id)}>X</button>
+                        </li>
                     })
                 }
 
