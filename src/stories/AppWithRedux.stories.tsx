37633918233216bbcad6fcf80de50be17eb3d2c5
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-03-10 21:20:57 +0300

    feat: created AppWithRedux.stories.tsx

---
+++ src/stories/AppWithRedux.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {AppWithRedux} from "../AppWithRedux";
+import {ReduxStoreProviderDecorator} from "./decorators/ReduxStoreProviderDecorator";
+
+export default {
+    title: 'TODO/AppWithRedux',
+    component: AppWithRedux,
+    decorators:[ReduxStoreProviderDecorator]
+} as ComponentMeta<typeof AppWithRedux>;
+
+const Template: ComponentStory<typeof AppWithRedux> = (args) => <AppWithRedux/>
+export const StoryAppWithRedux = Template.bind({});
+StoryAppWithRedux.args = {};
