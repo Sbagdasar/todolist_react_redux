a6163c0ffb344a45d08d899a95889a57eb214aee
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-03-30 22:09:53 +0300

    feat: added types for getTodolists method in todolistAPI

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-import { CreateTodolist, UpdateTodolistTitle } from '../stories/todolists-api.stories'
 let instance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.1/',
   withCredentials: true,
@@ -8,12 +7,6 @@ let instance = axios.create({
     'API-KEY': '1abf02da-1f01-4a57-9e95-d7c2e3ba6948',
   },
 })
-let settings = {
-  withCredentials: true,
-  headers: {
-    'API-KEY': '1abf02da-1f01-4a57-9e95-d7c2e3ba6948',
-  },
-}
 
 export const todolistAPI = {
   getTodolists() {
