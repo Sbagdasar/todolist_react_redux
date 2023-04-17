0a7322fd4c3a5781c4998f4dd445f4afa3bd7fac
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-04-17 22:09:50 +0300

    feat: added types for dispatch and thunk

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,16 +1,28 @@
-import { combineReducers, createStore } from 'redux'
+import { AnyAction, applyMiddleware, combineReducers, createStore } from 'redux'
+import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 
-import { tasksReducer } from './tasks-reducer'
-import { todolistsReducer } from './todolists-reducer'
+import { TasksActionType, tasksReducer } from './tasks-reducer'
+import { TodolistsActionType, todolistsReducer } from './todolists-reducer'
 
 const rootReducer = combineReducers({
   tasks: tasksReducer,
   todolists: todolistsReducer,
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
+
+type RootAppActions = TasksActionType | TodolistsActionType
+
+export type AppThunkType<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppRootStateType,
+  unknown,
+  RootAppActions
+>
+
+export type AppDispatchType = ThunkDispatch<AppRootStateType, unknown, RootAppActions>
 
 //@ts-ignore
 window.store = store
