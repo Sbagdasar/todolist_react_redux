2dc273325db6cc9fb458c87fa178f1b4ddcdb882
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-04-28 20:22:49 +0300

    feat: created ErrorSnackbar.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import './App.css'
 import MenuIcon from '@mui/icons-material/Menu'
+import { LinearProgress } from '@mui/material'
 import AppBar from '@mui/material/AppBar'
 import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
@@ -10,15 +11,22 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 
 import { TaskType } from '../api/todolist-api'
+import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar'
 import { TodoListsList } from '../features/TodolistsList/TodolistsList'
+import { useAppSelector } from '../hooks/appHooks'
+
+import { RequestStatusType } from './app-reducer'
 
 export type TasksStateType = {
   [key: string]: TaskType[]
 }
 
 export function App() {
+  const status = useAppSelector<RequestStatusType>(state => state.app.status)
+
   return (
     <div className="App">
+      <ErrorSnackbar />
       <AppBar position="static">
         <Toolbar>
           <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
@@ -29,6 +37,7 @@ export function App() {
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
+        {status === 'loading' && <LinearProgress />}
       </AppBar>
       <Container fixed>
         <TodoListsList />
